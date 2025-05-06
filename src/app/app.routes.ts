77bb73ddf90c49f311615dc03a17f5e6e122c7c4
ahmedabdelkawy63 <ah.abdelkawy63@gmail.com>
2025-05-06 15:45:18 +0200

    added details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,11 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { ConfigComponent } from './config/config.component'; 
+import { ConfigComponent } from './config/config.component';
 import { ProfileComponent } from './profile/profile.component';
-
+import { DetailsComponent } from './components/details/details.component';
 
 export const routes: Routes = [
-    { path: 'config', component: ConfigComponent },
-    { path: 'profile', component: ProfileComponent },
-
- 
-  
-  
-
+  { path: 'config', component: ConfigComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'movie/:id', component: DetailsComponent },
 ];
-
-
-
