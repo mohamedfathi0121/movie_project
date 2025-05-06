a4f0f9fb12faf9594434f3c620c7bbf6cd763417
محمد فتحي صلاح <130288085+mohamedfathi0121@users.noreply.github.com>
2025-05-07 00:32:04 +0300

    Route  small update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
-import { RedirectCommand, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { ConfigComponent } from './config/config.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { MoviesComponent } from './components/movies/movies.component';
 import { HomeComponent } from './components/home/home.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { MoviesCategoriesComponent } from './components/movies-categories/movies-categories.component';
