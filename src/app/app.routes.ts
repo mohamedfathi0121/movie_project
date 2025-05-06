cc29d79415868f45381e8318a54d60b7af199058
محمد فتحي صلاح <130288085+mohamedfathi0121@users.noreply.github.com>
2025-05-06 18:22:59 +0300

    update sidebar and add home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,21 @@
-import { Routes } from '@angular/router';
+import { RedirectCommand, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { ConfigComponent } from './config/config.component'; 
+import { ConfigComponent } from './config/config.component';
 import { ProfileComponent } from './profile/profile.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
     { path: 'config', component: ConfigComponent },
+    {path:'' ,redirectTo:'/home',pathMatch:'full'},
+    { path: 'home', component: HomeComponent },
     { path: 'profile', component: ProfileComponent },
+    {path:"**" , component: NotFoundComponent},
 
- 
-  
-  
+
+
+
 
 ];
 
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,14 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input,  } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule],
-
+  imports: [CommonModule,RouterLink,RouterLinkActive],
 
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
   @Input() isVisible = true;
+selectedSection: number = 1;
+  section: ISection[] = [
+    { name: '🏠 Home', id: 1 ,href:"/home"},
+    // { name: '🎬 Movies', id: 2 },
+    // { name: '📺 TV Series', id: 3 },
+    // { name: '📅 Upcoming', id: 4 },
+  ];
+  navigate(id: number) {
+this.selectedSection= id
+  }
+}
+interface ISection {
+  name: string;
+  id: number;
+  href: string;
 }
