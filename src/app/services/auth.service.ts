22ddec88819dcfeff4561cc20f22cb8b33e5c745
Haneen Khalil <haneenkhalil248@gmail.com>
2025-05-09 01:17:43 +0300

    firestore data

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,33 +5,71 @@ import {
   signInWithEmailAndPassword,
   signInWithPopup,
   GoogleAuthProvider,
-  signOut
+  signOut,
+  sendEmailVerification
+
 } from '@angular/fire/auth';
+import { Firestore, doc, setDoc, getDoc } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private auth = inject(Auth);
+  private firestore = inject(Firestore);
   private router = inject(Router);
 
-  // تسجيل مستخدم جديد
-  register(email: string, password: string) {
-    return createUserWithEmailAndPassword(this.auth, email, password);
+  register(email: string, password: string, name: string, phone: string, age: number, extraData?:any) {
+    return createUserWithEmailAndPassword(this.auth, email, password)
+      .then(async (userCredential) => {
+        const user = userCredential.user;
+
+        // إضافة بيانات في Firestore
+        await setDoc(doc(this.firestore, 'users', user.uid), {
+          uid: user.uid,
+          email: user.email,
+          name:name,
+          phone:phone,
+          age:age,
+
+        });
+
+        // إرسال إيميل تأكيد
+        await sendEmailVerification(user);
+
+        return userCredential;
+      });
   }
 
-  // تسجيل الدخول بالبريد وكلمة المرور
   login(email: string, password: string) {
     return signInWithEmailAndPassword(this.auth, email, password);
+
   }
 
-  // تسجيل الدخول باستخدام جوجل
   googleLogin() {
     const provider = new GoogleAuthProvider();
-    return signInWithPopup(this.auth, provider);
+    return signInWithPopup(this.auth, provider).then(async (result) => {
+      const user = result.user;
+      const userDocRef = doc(this.firestore, 'users', user.uid);
+      const userDoc = await getDoc(userDocRef);
+
+      // لو المستخدم جديد، ضيفه في Firestore
+      if (!userDoc.exists()) {
+        await setDoc(userDocRef, {
+          uid: user.uid,
+          email: user.email,
+          name: user.displayName || '',
+          photo: user.photoURL || ''
+        });
+      }
+    });
   }
 
-  // تسجيل الخروج
   logout() {
     return signOut(this.auth);
   }
+
+  isLogged(): boolean {
+    return !!this.auth.currentUser;
+  }
 }
+
