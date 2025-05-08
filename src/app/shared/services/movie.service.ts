5151421993758fe15be03f83144b903bafc3b72c
KerlosGirgis <kerlos0@protonmail.com>
2025-05-08 17:50:28 +0300

    added categories

--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -7,8 +7,7 @@ import { Movie } from '../../models/movie';
   providedIn: 'root',
 })
 export class MovieService {
-  private apiKey =
-    '7906db679229c246149e72a36a5f6fd4';
+  private apiKey = '7906db679229c246149e72a36a5f6fd4';
   private baseUrl = 'https://api.themoviedb.org/3/movie';
 
   constructor(private http: HttpClient) {}
@@ -27,10 +26,36 @@ export class MovieService {
     const url = `${this.baseUrl}/${category}`;
     const params = new HttpParams().set('api_key', this.apiKey);
 
-      return this.http.get<any>(url, { params }).pipe(
-        map(res => res.results.slice(0, limit))
-      );
-    
+    return this.http
+      .get<any>(url, { params })
+      .pipe(map((res) => res.results.slice(0, limit)));
   }
-  
+
+  getPagedMoviesByCategory(
+    category: 'now_playing' | 'popular' | 'top_rated' | 'upcoming',
+    page: number
+  ): Observable<{movies:Movie[], numOfPages:number}> {
+    const url = `${this.baseUrl}/${category}`;
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('page', page);
+    return this.http.get<any>(url, { params }).pipe(map((res) => ({movies:res.results,numOfPages:res.total_pages})));
+  }
+
+
+  getNumberOfPages(
+    category: 'now_playing' | 'popular' | 'top_rated' | 'upcoming'
+  ): Observable<number> {
+    const url = `${this.baseUrl}/${category}`;
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('page', 1);
+    return this.http.get<any>(url, { params }).pipe(
+      map((res) => {
+        const totalResults = res.total_results;
+        const totalPages = res.total_pages;
+        return totalPages;
+      }
+    ));
+  }
 }
