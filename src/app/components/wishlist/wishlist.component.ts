f3766989ba535b056dba9649435641369f356f37
aya <ayamoaz456@gmail.com>
2025-05-09 08:09:58 +0200

    save

--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MovieCardComponent} from './../../shared/components/movie-card/movie-card.component';
+import { Movie } from '../../models/movie';
+
 
 @Component({
   selector: 'app-wishlist',
-  imports: [],
+  standalone: true,
   templateUrl: './wishlist.component.html',
-  styleUrl: './wishlist.component.css'
+  styleUrls: ['./wishlist.component.css'],
+  imports: [MovieCardComponent,CommonModule],
 })
 export class WishlistComponent {
-
+  movie!: Movie;
 }
